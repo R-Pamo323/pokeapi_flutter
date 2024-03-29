60749b71c40600d4db100538ae4c19225d0e3a86
Rodrigo Goro <rodrigopamo323@gmail.com>
2024-03-29 13:24:28 -0500

    fetch: Agregando colors y nombre a los pokemons

--- lib/Controllers/Home/home_controller.dart
+++ lib/Controllers/Home/home_controller.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
   List pokedex = [];
+  dynamic? colorPoke;
 
   @override
   void onInit() {
@@ -28,10 +30,49 @@ class HomeController extends GetxController {
         if (kDebugMode) {
           print("$pokedex");
         }
-        //return data["pokemon"];
       }
     }).catchError((e) {
       print(e);
     });
+  }
+
+  dynamic setColor(int index) {
+    dynamic setColor;
+    switch (pokedex[index]["type"][0]) {
+      case "Grass":
+        setColor = Colors.greenAccent;
+        //setColor = Colors.blue;
+        break;
+      case "Fire":
+        setColor = Colors.redAccent;
+        //setColor = Colors.red;
+        break;
+      case "Water":
+        setColor = Colors.blue;
+        break;
+      case "Poison":
+        setColor = Colors.deepPurpleAccent;
+        //setColor = Colors.blue;
+        break;
+      case "Psychic":
+        setColor = Colors.indigo;
+        break;
+      case "Fighting":
+        setColor = Colors.orange;
+        break;
+      case "Bug":
+        setColor = Colors.lightGreenAccent;
+        //setColor = Colors.blue;
+        break;
+      case "Ghost":
+        setColor = Colors.deepPurple;
+        break;
+      case "Normal":
+        setColor = Colors.cyan;
+        break;
+      default:
+        setColor = Colors.pink;
+    }
+    return setColor;
   }
 }
--- lib/Views/Home/home_view.dart
+++ lib/Views/Home/home_view.dart
@@ -7,7 +7,166 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return GetBuilder<HomeController>(
-        init: HomeController(), builder: (_) => Scaffold(body: Container()));
+        init: HomeController(),
+        builder: (_) => Scaffold(
+                body: Container(
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 128, 64, 48),
+                  Color.fromARGB(255, 0, 0, 0)
+                ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: -50,
+                      right: -50,
+                      child: Image.asset(
+                        "assets/pokeball.png",
+                        width: 170,
+                        fit: BoxFit.fitWidth,
+                      )),
+                  Positioned(
+                    top: 100,
+                    left: 20,
+                    child: Text(
+                      "Pokedex",
+                      style: TextStyle(
+                          color: Colors.black12.withOpacity(0.6),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                  ),
+                  Positioned(
+                      top: 150,
+                      bottom: 0,
+                      width: width,
+                      child: Column(
+                        children: [
+                          _.pokedex.length != 0
+                              ? Expanded(
+                                  child: GridView.builder(
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio:
+                                        3 / 5, //*El ancho es 3 y el alto es 5
+                                  ),
+                                  itemCount: _.pokedex.length,
+                                  itemBuilder: (context, index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8, horizontal: 10),
+                                      child: InkWell(
+                                        //* Para que podamos hacer tap en una area
+                                        child: SafeArea(
+                                            //* Para que no afecte con los botones del android
+                                            child: Stack(
+                                          children: [
+                                            Container(
+                                              width: width,
+                                              margin: const EdgeInsets.only(
+                                                  top: 80),
+                                              decoration: BoxDecoration(
+                                                  color: Colors.black26,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(25))),
+                                              child: Stack(
+                                                children: [
+                                                  Positioned(
+                                                      top: 90,
+                                                      left: 15,
+                                                      child: Text(
+                                                        _.pokedex[index]["num"],
+                                                        style: const TextStyle(
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                            fontSize: 18,
+                                                            color:
+                                                                Colors.black87),
+                                                      )),
+                                                  Positioned(
+                                                      top: 130,
+                                                      left: 15,
+                                                      child: Text(
+                                                        _.pokedex[index]
+                                                            ["name"],
+                                                        style: const TextStyle(
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                            fontSize: 18,
+                                                            color:
+                                                                Colors.white54),
+                                                      )),
+                                                  Positioned(
+                                                      top: 170,
+                                                      left: 15,
+                                                      child: Container(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                .only(
+                                                                left: 10,
+                                                                right: 10,
+                                                                top: 5,
+                                                                bottom: 5),
+                                                        decoration: BoxDecoration(
+                                                            borderRadius:
+                                                                const BorderRadius
+                                                                    .all(Radius
+                                                                        .circular(
+                                                                            20)),
+                                                            color: Colors.black
+                                                                .withOpacity(
+                                                                    0.5)),
+                                                        child: Text(
+                                                          _.pokedex[index]
+                                                              ["type"][0],
+                                                          style: TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold,
+                                                              fontSize: 18,
+                                                              color: _.setColor(
+                                                                  index),
+                                                              shadows: [
+                                                                BoxShadow(
+                                                                    color: _
+                                                                        .setColor(
+                                                                            index),
+                                                                    offset:
+                                                                        Offset(
+                                                                            0,
+                                                                            0),
+                                                                    spreadRadius:
+                                                                        1.0,
+                                                                    blurRadius:
+                                                                        15)
+                                                              ]),
+                                                        ),
+                                                      ))
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        )),
+                                      ),
+                                    );
+                                  },
+                                ))
+                              : const Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                        ],
+                      ))
+                ],
+              ),
+            )));
   }
 }
